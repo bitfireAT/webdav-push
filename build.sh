87318b51a08c7388f00a1b522ffecdaa5ceeed10
Ricki Hirner <hirner@bitfire.at>
2025-05-03 14:08:39 +0200

    Refine header definition

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 mkdir -p build
-kramdown-rfc2629 webdav-push.mkd >build/webdav-push.xml && (cd build; xml2rfc --html webdav-push.xml && xml2rfc --text webdav-push.xml && xdg-open webdav-push.html)
+kramdown-rfc2629 webdav-push.mkd >build/webdav-push.xml && (cd build; xml2rfc --html webdav-push.xml && xml2rfc --text webdav-push.xml)
+
+echo To view in a browser, run:   xdg-open build/webdav-push.html
 
