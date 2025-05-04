7bbf5477d9261c0c6980907cf53f0841bf04a4e8
Ricki Hirner <hirner@bitfire.at>
2025-05-04 19:41:16 +0200

    Use xmlrfc <sourcecode> for HTTP messages / ABNF / XML

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p build
-kramdown-rfc2629 webdav-push.mkd >build/webdav-push.xml && (cd build; xml2rfc --html webdav-push.xml && xml2rfc --text webdav-push.xml)
+kramdown-rfc2629 -3 webdav-push.mkd >build/webdav-push.xml && (cd build; xml2rfc --html webdav-push.xml && xml2rfc --text webdav-push.xml)
 
 echo To view in a browser, run:   xdg-open build/webdav-push.html
 
